b68231896b4d159c40f2d69243eab89fe9af3a90
yannisjdubois <dub-y@tuta.io>
2022-06-18 17:02:41 -0400

    ajouts de fonctions publiques dans les modèles Event, Tag et User

--- app/Event.php
+++ app/Event.php
@@ -2,9 +2,24 @@
 
 namespace App;
 
+use App\Tag;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
     //
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+    
 }
--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use App\Event;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tag extends Model
 {
     //
+    use HasFactory;
+
+    public function events()
+    {
+        return $this->belongsToMany(Event::class);
+    }
+
+
+
+
+
 }
